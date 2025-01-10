62c7cb714092bca26f713cb6474f1fd019950e00
AshishChauhan0001 <asch20080@gmail.com>
2025-01-10 19:44:54 +0530

    deployment work

--- 07authentication/pages/api/auth/[...nextauth].jsx
+++ 07authentication/pages/api/auth/[...nextauth].jsx
@@ -1,12 +1,14 @@
 import NextAuth from "next-auth";
 import GithubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
+import GoogleProvider from 'next-auth/providers/linkedin';
 
 export const authOPtions={
     providers:[
         GithubProvider({
-            clientId:'',
-            clientSecret:''
-        })
-    ]
-}
+            clientId:process.env.CLIENT_ID,
+            clientSecret:process.env.CLIENT_SECRET
+        }),
+    ],
+};
+
+export default NextAuth(authOPtions);
--- 07authentication/pages/index.js
+++ 07authentication/pages/index.js
@@ -1,13 +1,20 @@
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
+import { useSession,signIn,signOut } from "next-auth/react";
 
 
 
 export default function Home() {
+  const session =useSession();
+  console.log("Session : ",session);
+  if(session.data===null) return <button onClick={signIn}>Login</button>
+  
+  
   return (
     <>
       <div>
-        <h1>Auth</h1>
+        <h1> Hello {session?.data?.user?.name}</h1>
+        <button onClick={signOut}>Logout</button>
       </div>
     </>
   );
